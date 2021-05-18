a2406cc2e77d3e0cb7e07cbee7d17ff8ec76a8e5
Hunter <83977497+Hunter-Serbenta@users.noreply.github.com>
2021-05-18 13:52:35 -0500

    Add files via upload

---
+++ Final Game/src/Boss.java
@@ -0,0 +1,16 @@
+package workplsgame;
+
+import info.gridworld.actor.Actor;
+import info.gridworld.grid.Location;
+
+public class Boss extends Actor
+{
+	public void turn() {
+		int count = 0;
+		count ++;
+		if (count %10 == 0)
+			setDirection(getDirection() + Location.RIGHT);
+		else
+			setDirection(getDirection() + Location.HALF_RIGHT);
+	}
+}
---
+++ Final Game/src/Dog.java
@@ -0,0 +1,29 @@
+package workplsgame;
+
+import info.gridworld.actor.*;
+
+import info.gridworld.grid.Grid;
+import info.gridworld.grid.Location;
+
+import java.awt.Color;
+
+public class Dog extends Actor
+{
+	public Dog()
+	{
+		setColor(null);
+	}
+	public void act()
+	{
+		turn();
+	}
+	
+	private void turn() {
+		int count = 0;
+		count ++;
+		if (count %10 == 0)
+			setDirection(getDirection() + Location.RIGHT);
+		else
+			setDirection(getDirection() + Location.HALF_RIGHT);
+	}
+}
