091e0e229650f2ab324981c245afeb55b2ced596
zapjoshua <zapjoshua@live.com>
2021-05-20 12:50:14 -0500

    Update Game.java

--- Final Game/src/Game.java
+++ Final Game/src/Game.java
@@ -1,23 +1,65 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import info.gridworld.actor.*;
+import info.gridworld.actor.Actor;
 import info.gridworld.actor.ActorWorld;
-import info.gridworld.actor.Rock;
-import info.gridworld.grid.BoundedGrid;
-import info.gridworld.grid.Location;
+import info.gridworld.grid.*;
 
-import java.awt.Color;
-
-/**
- * This class runs a world that contains box bugs. <br />
- * This class is not tested on the AP CS A and AB exams.
- */
-public class Game
+public class Game extends Actor
 {
-    public static void main(String[] args)
-    {
-    	World2 w2 = new World2();
-    	
-    	BoundedGrid grid = new BoundedGrid(20, 20);
-        //ActorWorld world = new ActorWorld();
-        w2.setGrid(grid);
-        w2.show();
+   public static ActorWorld world = new ActorWorld(new BoundedGrid<Actor>(20, 20));
+   
+   public static void main(String[] args) 
+   {
+	   //Hiding unimportant things and changing name from GridWorld to Rat Game
+       System.setProperty("info.gridworld.gui.selection", "hide");
+       System.setProperty("info.gridworld.gui.tooltips", "hide");
+       System.setProperty("info.gridworld.gui.frametitle", "The Game");
+       
+       //Test Popup for fighting sequence
+       JFrame text = new JFrame();
+
+       //Adding Rat to the world
+	   Player r = new Player();
+	   world.add(new Location(10,10),r);
+	   world.add(new Location (5,5),new Rock());
+       world.show();
+       
+       //Keyboard inputs
+       java.awt.KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(new java.awt.KeyEventDispatcher() 
+       {
+    	   public boolean dispatchKeyEvent(java.awt.event.KeyEvent event) 
+    	   {
+    		   String key = javax.swing.KeyStroke.getKeyStrokeForEvent(event).toString();
+    		   //up
+    		   if (key.equals("pressed W"))
+    			   r.moveTo(r.getLocation().getAdjacentLocation(0));
+    		   //down
+    		   if (key.equals("pressed D"))
+    			   r.moveTo(r.getLocation().getAdjacentLocation(Location.EAST));
+    		   //left
+    		   if (key.equals("pressed S"))
+    			   r.moveTo(r.getLocation().getAdjacentLocation(Location.SOUTH));
+    		   //right
+    		   if (key.equals("pressed A"))
+    			   r.moveTo(r.getLocation().getAdjacentLocation(Location.WEST));
+    		   //pick up
+    		   if (key.equals("pressed E"))
+    			   JOptionPane.showMessageDialog(text, "You grabbed an item");
+    		   //fight
+    		   if (key.equals("pressed F"))
+    			   JOptionPane.showMessageDialog(text, "FIGHT");
+    		   world.show();
+    		   return true;
+    	   }
+       });
+       
+       //Shows message above game window
+       world.setMessage("Its Gaming Time");      
+       world.show();
     }
 }
