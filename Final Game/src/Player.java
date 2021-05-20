bcfc8c3e91348d6240e9677812db32aa0a5b7636
zapjoshua <zapjoshua@live.com>
2021-05-20 12:45:58 -0500

    Create Player.java

---
+++ Final Game/src/Player.java
@@ -0,0 +1,20 @@
+import info.gridworld.actor.*;
+
+import info.gridworld.grid.Grid;
+import info.gridworld.grid.Location;
+
+import java.awt.Color;
+import java.util.Scanner;
+
+public class Player extends Actor
+{
+	public Player()
+	{
+		setColor(null);
+	}
+	
+	public void Act()
+	{
+		
+	}
+}
