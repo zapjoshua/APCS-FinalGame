b060ca9f964e93cf4e279df29d808570c9a96d58
Hunter <83977497+Hunter-Serbenta@users.noreply.github.com>
2021-05-24 13:34:40 -0500

    Update Door.java

--- Final Game/src/Door.java
+++ Final Game/src/Door.java
@@ -8,9 +8,23 @@ import java.awt.Color;
 
 public class Door extends Actor
 {
+	
+	protected boolean open;
+	
 	public Door()
 	{
 		setColor(null);
+		open = false;
+	}
+	
+	public void Open()
+	{
+		open = true; 
+	}
+	
+	public void Close()
+	{
+		open = false;
 	}
 }
 
