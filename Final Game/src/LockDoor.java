b96263786a1f77ec7575f24c60ee274cb76269b7
Hunter <83977497+Hunter-Serbenta@users.noreply.github.com>
2021-05-24 13:35:01 -0500

    Update LockDoor.java

--- Final Game/src/LockDoor.java
+++ Final Game/src/LockDoor.java
@@ -1,4 +1,39 @@
 
-public class LockDoor {
-
+public class LockDoor extends Door
+{
+	private boolean lock;
+	public int numKeys = 0;
+	
+	public LockDoor()
+	{
+		lock = true;
+	}
+	
+	public void Lock()
+	{
+		if (numKeys != 3)
+		{
+			if(open == false)
+			{
+				lock = true;
+			}
+			else
+				System.out.print("You don't have all the keys!");
+		}
+	}
+	
+	public void UnLock()
+	{
+		if (numKeys >= 3)
+		{
+			if(lock = true)
+			{
+				lock = false;
+				open = true;
+			}
+			else
+				System.out.print("You have already gathered all the keys!");
+			
+		}
+	}	
 }
