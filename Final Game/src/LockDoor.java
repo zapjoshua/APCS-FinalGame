bf67579e13e636e363f203acd7fdc7c2140c71a8
ArduinoMaster <gamegirl900.sp@gmail.com>
2021-05-24 13:42:05 -0500

    Update LockDoor.java

--- Final Game/src/LockDoor.java
+++ Final Game/src/LockDoor.java
@@ -1,4 +1,11 @@
+import java.io.File;
+import java.io.IOException;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 public class LockDoor extends Door
 {
 	private boolean lock;
@@ -9,6 +16,19 @@ public class LockDoor extends Door
 		lock = true;
 	}
 	
+	public static void play(String filename)
+	{
+	    try
+	    {
+	        Clip clip = AudioSystem.getClip();
+	        clip.open(AudioSystem.getAudioInputStream(new File(filename)));
+	        clip.start();
+	    }
+	    catch (Exception exc)
+	    {
+	        exc.printStackTrace(System.out);
+	    }
+	}
 	public void Lock()
 	{
 		if (numKeys != 3)
@@ -16,6 +36,7 @@ public class LockDoor extends Door
 			if(open == false)
 			{
 				lock = true;
+				play("DoorLock.wav");
 			}
 			else
 				System.out.print("You don't have all the keys!");
@@ -30,6 +51,7 @@ public class LockDoor extends Door
 			{
 				lock = false;
 				open = true;
+				play("DoorLock.wav");
 			}
 			else
 				System.out.print("You have already gathered all the keys!");
