9da15db61c376ad7ec79150317a4f4735a50340b
Hunter <83977497+Hunter-Serbenta@users.noreply.github.com>
2021-05-24 13:31:48 -0500

    Add files via upload

---
+++ Final Game/src/Door.java
@@ -0,0 +1,16 @@
+
+import info.gridworld.actor.*;
+
+import info.gridworld.grid.Grid;
+import info.gridworld.grid.Location;
+
+import java.awt.Color;
+
+public class Door extends Actor
+{
+	public Door()
+	{
+		setColor(null);
+	}
+}
+
